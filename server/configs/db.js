cba16e170525408f8419c1fc5b553b6f4ddd5578
TranThiThanhPhuong <phuongsuga333@gmail.com>
2025-08-27 16:42:45 +0700

    27/8

--- server/configs/db.js
+++ server/configs/db.js
@@ -6,10 +6,9 @@ const connectDB = async () => {
     mongoose.connection.on('connected', () => console.log('Database connected'));
 
     // Sử dụng biến môi trường MONGO_URI để kết nối đến MongoDB
-    await mongoose.connect(`${process.env.MONGO_URI}`);
+    await mongoose.connect(`${process.env.MONGO_URI}/pbl6_db`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
   }
 };
 
